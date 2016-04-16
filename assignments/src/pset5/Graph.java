b98c93684ba050f110b680fd6be63301b2f39bf3
Eric Crosson <esc@ericcrosson.com>
2016-04-16 11:27:47 -0500

    initial implementation of pset5
    tests following shortly

---
+++ assignments/src/pset5/Graph.java
@@ -0,0 +1,91 @@
+package pset5;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Graph {
+
+    /** Set of nodes in the graph */
+    private Set<Integer> nodes;
+    /** Map of each node and the set of nodes connected to it by outgoing edges */
+    private Map<Integer, TreeSet<Integer>> edges;
+
+    /** Class invariant: fields {@code nodes} and {@code edges} are non-null */
+    public Graph() {
+        nodes = new TreeSet<Integer>();
+        edges = new TreeMap<Integer, TreeSet<Integer>>();
+    }
+
+    /** Return a String representation of this graph */
+    public String toString() {
+        return String.format("nodes: %s\nedges: %s", 
+                             nodes.toString(), edges.toString());
+    }
+    
+    /** Add the node {@code n} to this graph */
+    public void addNode(int n) {
+        nodes.add(n);
+    }
+
+    /** Add a directed edge {@code from} -> {@code to} to this graph */
+    public void addEdge(int from, int to) {
+        addNode(from);
+        addNode(to);
+        TreeSet<Integer> neighbors = edges.get(from);
+        if (neighbors == null) {
+            neighbors = new TreeSet<Integer>();
+        }
+        neighbors.add(to);
+        edges.put(from, neighbors);
+    }
+
+    /** True if
+     *  - {@code sources} is a subset of {@code nodes}, and
+     *  - {@code targets} is a subset of {@code nodes}, and
+     *  - there is some node {@code n} in {@code sources} such that there is a
+     *          directed path that starts at {@code n} and ends at {@code m} in
+     *          {@code this}
+     */
+    public boolean reachable(Set<Integer> sources, Set<Integer> targets) {
+        if (sources == null || targets == null) throw new IllegalArgumentException();
+        return (subsetOfNodes(sources) && subsetOfNodes(targets) && 
+                connectionToThis(sources, targets));
+    }
+
+    /** True if {@code set} is a subset of {@code this.nodes} */
+    private boolean subsetOfNodes(Set<Integer> set) {
+        Set<Integer> unfound = new TreeSet<Integer>();
+        for (int find : set) {
+            if (!nodes.contains(find)) {
+                unfound.add(find);
+            }
+        }
+        return unfound.size() == 0;
+    }
+
+    /** True if {@code from} contains a node {@code n} that connects to {@code
+     * to} */
+    private boolean connectionToThis(Set<Integer> from, Set<Integer> to) {
+        for (int m : from) {
+            for (int n : to) {
+                if (search(m, n)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /** True if node {@code start} has a connection to node {@code target} */
+    private boolean search(int start, int target) {
+        if (start == target) { return true; }
+        for (int s : edges.get(start)) {
+            if (search(s, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
